b118721e216727c625912903e065473254e7086e
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-01-13 15:15:51 +0800

    Add cbi model

---
+++ luasrc/model/cbi/autossh.lua
@@ -0,0 +1,23 @@
+model = Map("autossh", translate("AutoSSH"), translate("Automatically restart SSH sessions and tunnels"))
+section = model:section(TypedSection, "autossh", translate("AutoSSH Configurations"))
+
+enabled = section:option(Flag, "enabled", translate("enabled"));
+enabled.optional = false;
+
+ssh = section:option(Value, "ssh", translate("SSH"));
+ssh.optional = false;
+ssh.rmempty = false;
+
+gatetime = section:option(Value, "gatetime", translate("gatetime"));
+gatetime.optional = false;
+gatetime.rmempty = false;
+
+monitorport = section:option(Value, "monitorport", translate("monitorport"));
+monitorport.optional = false;
+monitorport.rmempty = false;
+
+poll = section:option(Value, "poll", translate("poll"));
+poll.optional = false;
+poll.rmempty = false;
+
+return model
